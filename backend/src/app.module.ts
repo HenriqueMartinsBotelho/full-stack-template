047ab23b728d657ff7a8f3ee435584ed61c3027c
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2025-06-02 20:56:59 -0300

    e

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
     AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
